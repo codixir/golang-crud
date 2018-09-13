7240ab39607feb2292074f3b48a35642df945220
codixir <codixir@gmail.com>
2018-09-13 06:40:09 -0400

    Create router and register endpoints and handler fucntions

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     int    `json:id`
+	Title  string `json:title`
+	Author string `json:author`
+	Year   string `json:year`
+}
+
+var books []Book
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+}
